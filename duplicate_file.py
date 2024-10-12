b108cf3344c485c82448f9f025c120ba600013e4
Harsh <vanetiaharsh@gmail.com>
2024-10-12 18:07:01 +0530

    final commit

--- duplicate_file.py
+++ duplicate_file.py
@@ -14,8 +14,13 @@ def main():
     duplicate_files = compare(files)
 
     #show dup files
+    for path in duplicate_files:
+        print(f"{os.path.basename(path)} at {path}")
 
     #allow to delete files
+    choice = input("Press 'y' to delete duplicate files: ")
+    if choice.strip().lower() == "y":
+        delete(duplicate_files)
 
 
 def get_path():
@@ -29,7 +34,7 @@ def process(path):
 
     for (root,dirs,files) in os.walk(path):
         for file in files :
-            full_path = os.path.join(path, file)
+            full_path = os.path.join(root, file)
             file_size = os.path.getsize(full_path)
             if file_size in seen_files:
                 seen_files[file_size].append(full_path)
@@ -45,14 +50,21 @@ def compare(files):
 
     for size in files:
         if len(files[size]) > 1:
-            for file in files[size]:
+            for i, file in enumerate(files[size]):
                 refrence_file = file
-                # to be continue
+                for file in files[size][i+1:]:
+                    if filecmp.cmp(refrence_file, file, shallow=False):
+                        duplicate_files.append(file)
 
+    return set(duplicate_files)
 
-    
-            
-
+def delete(files):
+    for file in files:
+        try:
+            os.remove(file)
+            print(f"Deleted: {os.path.basename(file)}")
+        except Exception as e:
+            print(f"Error deleting {os.path.basename(file)}: {e}")
 
 if __name__ == "__main__":
     main() 
